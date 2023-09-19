bf765b5291eb9db97826958d1a728a4054e26de2
Rilo <taydh.wade@gmail.com>
2023-09-19 14:45:38 -0700

    troubleshooting keyframes

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const body = document.querySelector('body')
+const sliderInput = document.querySelector('#slider-input')
+const allBoxes = document.querySelectorAll('.box')
+const allPills = document.querySelectorAll('.pill');
+const allHiddenPills = document.querySelectorAll('.hidden-pill');
+const allArrows = document.querySelectorAll('.arrow');
+const expandPill = document.querySelector('#expand');
+const xLetter = document.querySelector('#x-letter');
+const xletterSVG = document.querySelector('#x-letter-svg');
+const xBox = document.querySelector('.x-box');
+const socialFan = document.querySelector('.social-fan');
+const reversePill = document.querySelector('#reverse');
+const boxContainer = document.querySelector('.box-contianer');
+const iconSVG = document.querySelector()
+
